932faade055a0367d9aa81f57646c36d58273ec9
Klaudia Dziubek <kdziubek@happcode.com>
2022-10-10 16:57:32 +0100

    Code extracted to separate functions, refactored

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+def removeDuplicatesAndSortDesc(listOfDigits: list) -> list:
+    # Delete duplications from the list
+    newList = removeDuplicates(listOfDigits)
+    # Sort list in descending order
+    newList.sort(reverse=True)
+    return newList
+
+def removeDuplicates(listOfDigits: list) -> list:
+    newList = []
+    for n in listOfDigits:
+        if n not in newList:
+            newList.append(n)
+    return newList
+
+# CODE EXAMPLE
+
+
 # Read the list of 10 digits in range 1-100
 digits = [1, 2, 2, 4, 5, 6, 7, 8, 9, 9]
-# Delete duplications from the list
-newDigits = []
-for n in digits:
-    if n not in newDigits:
-        newDigits.append(n)
-
-# Sort list in descending order
-newDigits.sort(reverse=True)
-
-# Display output by calling function
+print(removeDuplicatesAndSortDesc(digits))
